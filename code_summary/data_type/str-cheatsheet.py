e3550b3a4247c7d66ea154c49b59f8acd0a4b8bd
cccarly <yitangcarly@gmail.com>
2023-01-03 22:58:50 -0500

    Update str-cheatsheet.py

--- code_summary/data_type/str-cheatsheet.py
+++ code_summary/data_type/str-cheatsheet.py
@@ -1,3 +1,47 @@
 ### sequence of characters
 ### with doubt or single quote
+### ordered,ummutable, text representation
 
+
+
+### when string with ' 
+
+x = 'i\'m a prgrammer'
+x = "i'm a programmer"
+print(x)
+# escaping backslash
+my_string = 'I\' m  a "Geek"'
+my_string = 'I\' m a \'Geek\''
+print(my_string)
+### triple quotes works too!
+### immutable !!!!
+### slicing available such as list 
+### :: also works, ::-1 tricks also works 
+### works with plus concatenate 
+i = 'snow'
+combine = x + " " + i
+print(combine)
+
+### iterator works <for> - check if char inside of string using <if>
+
+### access and substrings 
+
+
+
+###  remove white space 
+b = '        hello'
+print(b.strip())
+### immutable, the original b doesn't change, assign new value = b.strip() works 
+print(b)
+
+## binary true false
+print(x.startswith('pr'))
+
+## find 
+print(x.find('pr'))
+
+## count 
+
+## string with replace 
+x.replace("programmer","bankser")
+print(x.replace("programmer","bankser"))
